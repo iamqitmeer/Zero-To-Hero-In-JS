3abbf46b383c4d9c04f7a2fc64bc70a5427efe96
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-07-05 14:08:07 +0500

    learning async

--- Topics Cover With Some Tricky Questions Questions/Advance/Mastering Asynchronous JavaScript/index.js
+++ Topics Cover With Some Tricky Questions Questions/Advance/Mastering Asynchronous JavaScript/index.js
@@ -1,4 +1,49 @@
+// Understanding What is Async Javascript?
+
+// ---> Sync
+// console.log("A");
+// console.log("B");
+// console.log("C");
+
+// ---> Async
+
+// console.log("A (Sync Code)");
+// console.log("B (Sync Code)");
+//   setTimeout(() => {
+//     console.log("C (Async Code)");
+//   }, 0);
+// console.log("D (Sync Code)");
+
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+
 // CallBacks
+
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+
+let isUserLoggedIn = true;
+
+function greetAfterLoggedIn() {
+  let user = {
+    name: "Qitmeer Raza",
+    email: "iamqitmeer",
+  };
+  console.log(`Good Morning ${user.name}.`);
+}
+
+function loggedInMsg(callback) {
+  if (isUserLoggedIn) {
+    setTimeout(() => {
+      console.log("You Are Logged In");
+      callback();
+    }, 1000);
+  }
+}
+
+loggedInMsg(greetAfterLoggedIn);
 
 // function logIn(callback) {
 //   setTimeout(() => {
@@ -20,7 +65,15 @@
 
 // logIn(userInformation);
 
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+
 // Promise
+
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
 
 // let newPromise = new Promise((res, rej) => {
 //   if (5 == "1") {
@@ -66,55 +119,63 @@
 //   console.log(err);
 // });
 
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+
 // Fetch
 
-function getDataFromApi(callback) {
-  fetch(`https://fakestoreapi.com/products`)
-    .then((res) => res.json())
-    .then((data) => callback(data));
-}
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
 
-function displayData(val) {
-  console.log(val);
+// function getDataFromApi(callback) {
+//   fetch(`https://fakestoreapi.com/products`)
+//     .then((res) => res.json())
+//     .then((data) => callback(data));
+// }
 
-  val.forEach((item) => {
-    console.log(item);
-    let mainElem = document.getElementById("mainElem");
-    let elem = `<div
-                class="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96"
-              >
-                <div
-                  class="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40"
-        >
-          <img
-            src="${item.image}"
-            alt="card-image"
-          />
-        </div>
-        <div class="p-6">
-          <h5
-            class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900"
-          >
-          ${item.title}
-          </h5>
-          <p
-            class="block font-sans text-base antialiased font-light leading-relaxed text-inherit"
-          >
-          ${item.description}
-          </p>
-        </div>
-        <div class="p-6 pt-0">
-          <button
-            class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
-            type="button"
-          >
-            Read More
-          </button>
-        </div>
-      </div>`;
+// function displayData(val) {
+//   console.log(val);
 
-    mainElem.innerHTML += elem;
-  });
-}
+//   val.forEach((item) => {
+//     console.log(item);
+//     let mainElem = document.getElementById("mainElem");
+//     let elem = `<div
+//                 class="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96"
+//               >
+//                 <div
+//                   class="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40"
+//         >
+//           <img
+//             src="${item.image}"
+//             alt="card-image"
+//           />
+//         </div>
+//         <div class="p-6">
+//           <h5
+//             class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900"
+//           >
+//           ${item.title}
+//           </h5>
+//           <p
+//             class="block font-sans text-base antialiased font-light leading-relaxed text-inherit"
+//           >
+//           ${item.description}
+//           </p>
+//         </div>
+//         <div class="p-6 pt-0">
+//           <button
+//             class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+//             type="button"
+//           >
+//             Read More
+//           </button>
+//         </div>
+//       </div>`;
 
-getDataFromApi(displayData);
+//     mainElem.innerHTML += elem;
+//   });
+// }
+
+// getDataFromApi(displayData);
