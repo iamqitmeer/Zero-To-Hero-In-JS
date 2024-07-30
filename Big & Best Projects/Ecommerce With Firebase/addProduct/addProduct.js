9686f72f8884f71c597debd313737791f6b708e4
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-07-30 22:03:28 +0500

    complete

--- Big & Best Projects/Ecommerce With Firebase/addProduct/addProduct.js
+++ Big & Best Projects/Ecommerce With Firebase/addProduct/addProduct.js
@@ -1,7 +1,8 @@
 import {
   ref,
   storage,
-  setDoc,db,
+  setDoc,
+  db,
   getDoc,
   uploadBytes,
   collection,
@@ -18,35 +19,52 @@ let stocks = document.getElementById("stocks");
 let category = document.getElementById("category");
 
 addProduct.addEventListener("click", () => {
-    let productData = {
-      productImage: image.files[0], // Get the first file from the input
-      title: title.value,
-      description: description.value,
-      price: price.value,
-      stocks: stocks.value,
-      category: category.value,
-    };
-  
-    if (productData.productImage) {
-      let productImageRef = ref(storage, 'images/' + productData.productImage.name); // Specify a non-root reference
-  
-      uploadBytes(productImageRef, productData.productImage).then(() => {
+  addProduct.disabled = true;
+  addProduct.innerText = "Loading...";
+
+  let productData = {
+    productImage: image.files[0], // Get the first file from the input
+    title: title.value,
+    description: description.value,
+    price: price.value,
+    stocks: stocks.value,
+    category: category.value,
+  };
+
+  if (productData.productImage) {
+    let productImageRef = ref(
+      storage,
+      "images/" + productData.productImage.name
+    ); // Specify a non-root reference
+
+    uploadBytes(productImageRef, productData.productImage)
+      .then(() => {
         console.log("File Uploaded");
-  
+
         getDownloadURL(productImageRef).then((url) => {
           console.log("Url --->", url);
           productData.productImage = url;
-  
+
           let prodCollection = collection(db, "product");
           addDoc(prodCollection, productData).then((snapshot) => {
             console.log("Doc Added -->", snapshot);
+          
+            addProduct.disabled = false;
+          addProduct.innerText = "Save Product";
           });
+          
         });
-      }).catch((error) => {
+      })
+      .catch((error) => {
         console.error("Error uploading file:", error);
+
+        addProduct.disabled = false;
+        addProduct.innerText = "Save Product";
       });
-    } else {
-      console.error("No file selected for upload.");
-    }
-  });
-  
+  } else {
+    console.error("No file selected for upload.");
+
+    addProduct.disabled = false;
+        addProduct.innerText = "Save Product";
+  }
+});
