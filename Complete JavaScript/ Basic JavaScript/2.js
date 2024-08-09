526e7d0acd1635eee025ce87a7e736c525655042
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-08-09 12:56:04 +0500

    Basic Changes

--- Complete JavaScript/ Basic JavaScript/2.js
+++ Complete JavaScript/ Basic JavaScript/2.js
@@ -1,3 +1,14 @@
-// Varieble is lika a memory box. `var` ye memory banane ka tareeqa he = ke baad jo value likhenge wo us 
+// Varieble is lika a memory box. `var` ye memory banane ka tareeqa he = ke baad jo value likhenge wo us memory ka data he.
 
-let variebleDec = "Qitmeer Raza";
+var variebleDec = "Qitmeer Raza";
+
+// varieble banane ka pehle 1 tareeqa tha `var`
+
+// ab es6 ke baad 2 tareeqe or aye he
+
+// `let` ye modern or sab se zyada use hone wali he...
+let myName = "Qitmeer"
+// `const` ye bhi modern or constant varieble banene ke liye use hoti he.
+const num = 12
+
+// detailed difference aage ke folders me dekhenge
