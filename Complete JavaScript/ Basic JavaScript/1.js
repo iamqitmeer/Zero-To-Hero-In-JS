d9d5b873360d1dd7f75536b6563519275f4e01e6
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-08-09 12:18:49 +0500

    Add Comment and Varieble notes

---
+++ Complete JavaScript/ Basic JavaScript/1.js
@@ -0,0 +1,11 @@
+// Comment Your JavaScript Code
+
+// Agar Ham Koi Aisa Kaam Kr rahe he jo ham chahte he ke wo hamara compiler ignor kre e.g. Code ko Describe krna etc
+
+// I Create Varieble --> Comment Using //
+// let num = 1
+// console.log(num);
+
+/*
+let number = 15;
+console.log(number); */         /* --> Comment Using  */
---
+++ Complete JavaScript/ Basic JavaScript/2.js
@@ -0,0 +1,3 @@
+// Varieble is lika a memory box. `var` ye memory banane ka tareeqa he = ke baad jo value likhenge wo us 
+
+let variebleDec = "Qitmeer Raza";
